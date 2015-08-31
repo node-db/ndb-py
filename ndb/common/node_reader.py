b8b86ae71155bc6e00c073bbc00a5a04d6037f78
Yugeng Hui <interhuiyg@163.com>
2015-08-31 13:20:24 +0800

    node_reader Bug Fixed

--- ndb/common/node_reader.py
+++ ndb/common/node_reader.py
@@ -47,43 +47,47 @@ class NodeReader():
 
         while self.linenum < len(_list):
             
-            line = _list[self.linenum].strip()
-    
-            self.linenum = self.linenum + 1
-            
-            # 过滤注释信息，注释信息以#开头
-            if line == None or line == '' or line.startswith('#'):
-                continue
-            
-            # 如果行中含有'{' 则将下面的内容进行递归
-            if line.endswith('{'):
-                node_name = line[0: line.find('{')].strip()
-                node_value = self.__parse(_list)
-                sub_node = node.get(node_name)
+            try:
+                line = _list[self.linenum].strip()
+        
+                self.linenum = self.linenum + 1
                 
-                if sub_node == None:
-                    node[node_name] = node_value
-                else:
-                    node_list = sub_node if type(sub_node) == types.ListType else [sub_node]
-                    node_list.append(node_value)
-                    node[node_name] = node_list
-            
-            # 如果行中含有 '}' 则返回解析后的内容        
-            elif line.endswith('}'):
-                return node
-            
-            else:    
-                if ':' in line:
-                    node_name = line[0: line.find(':')].strip()
-                    item_value = line[line.find(':') + 1: len(line)].strip()
+                # 过滤注释信息，注释信息以#开头
+                if line == None or line == '' or line.startswith('#'):
+                    continue
                 
-                node_value = node.get(node_name) if node_name != None else None
+                # 如果行中含有'{' 则将下面的内容进行递归
+                if line.endswith('{'):
+                    node_name = line[0: line.find('{')].strip()
+                    node_value = self.__parse(_list)
+                    sub_node = node.get(node_name)
+                    
+                    if sub_node == None:
+                        node[node_name] = node_value
+                    else:
+                        node_list = sub_node if type(sub_node) == types.ListType else [sub_node]
+                        node_list.append(node_value)
+                        node[node_name] = node_list
                 
-                if node_value != None:
-                    item_value_list = node_value if type(node_value) == types.ListType else [node_value]
-                    item_value_list.append(item_value)   
-                    node[node_name] = item_value_list
-                else:
-                    node[node_name] = item_value
+                # 如果行中含有 '}' 则返回解析后的内容        
+                elif line.endswith('}'):
+                    return node
+                
+                else:    
+                    if ':' in line:
+                        node_name = line[0: line.find(':')].strip()
+                        item_value = line[line.find(':') + 1: len(line)].strip()
+                    
+                        if node_name != None and node_name !="":
+                            node_value = node.get(node_name) if node_name != None else None
+                        
+                        if node_value != None:
+                            item_value_list = node_value if type(node_value) == types.ListType else [node_value]
+                            item_value_list.append(item_value)   
+                            node[node_name] = item_value_list
+                        else:
+                            node[node_name] = item_value
+            except:
+                pass
         
         return node
