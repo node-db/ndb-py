058a0eb16a5a807c137553042f973c907d231cf1
Yugeng Hui <interhuiyg@163.com>
2016-01-19 01:03:08 +0800

    Add clean and travel to operate

--- ndb/operate/__init__.py
+++ ndb/operate/__init__.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 
 import node_locate
-import node_filter
 
 import node_select
 import node_update
 import node_delete
 import node_insert
 import node_script
+import node_clean
+import node_travel
 
 def select(node, path, action=None):
     return node_select.NodeSelect().select(node, path, action)
@@ -24,8 +25,12 @@ def insert(node, path, value, action=None):
 def locate(node, query, multi, is_create=False):
     return node_locate.NodeLocate().locate(node, query, multi, is_create)
 
+def clean(node):
+    return node_clean.NodeClean().clean(node)
+
+def travel(node, action):
+    return node_travel.NodeTravel().travel(node, action)
+
 def script(node, script_filename):
     return node_script.NodeScript().run(node, script_filename)
 
-def filte(table, query=None, union=False, sort_key=None):
-    return node_filter.NodeFilter().filte(table, query, union, sort_key)
